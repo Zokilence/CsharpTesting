df566c50afe04f9b2272ff9b579995a8cef67356
Zokilence <zokicerevic@gmail.com>
2021-12-25 21:19:43 +0100

    startConfigs

---
+++ CSharpTestingThreads/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CSharpTestingThreads
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
