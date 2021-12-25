8f26f665db81d24fa0d0ce421e432a68afaed31b
Zokilence <zokicerevic@gmail.com>
2021-12-25 21:14:55 +0100

    Add project files.

---
+++ CSharpTesting/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CSharpTesting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
