0f3c49c00aba93db6a6120b82dd5c2b07a86f186
Zokilence <zokicerevic@gmail.com>
2021-12-30 08:36:47 +0100

    multidimenzionalArray

---
+++ arrays/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] niz =
+            { 
+                {1,2 },
+                {3,4 },
+                {5,6 } 
+            }; 
+            foreach (var item in niz)
+            {
+                Console.WriteLine(item.ToString());
+            }
+        }
+    }
+}
