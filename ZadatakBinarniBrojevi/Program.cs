67d7517e0c4bbea74efdfb4a3501bb335cac5d3f
Zokilence <zokicerevic@gmail.com>
2021-12-31 00:36:00 +0100

    KonacnoV1

--- ZadatakBinarniBrojevi/Program.cs
+++ ZadatakBinarniBrojevi/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 
 namespace ZadatakBinarniBrojevi
 {
@@ -12,8 +11,9 @@ namespace ZadatakBinarniBrojevi
             // 5 -> 101b , 6 -> 110b ,  7-> 111b , 8 -> 1000b
 
             //+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=
+
             //skripta koja u niz dodaje kvadrate dvojke
-            int[] kvadratiDva = new int[16];
+            int[] kvadratiDva = new int[25];
             kvadratiDva[0] = 1;
             int povecavanjeKvadrata = 1;
             for (int i = 1; i < kvadratiDva.Length; i++)
@@ -26,64 +26,75 @@ namespace ZadatakBinarniBrojevi
                 kvadratiDva[i] = a;
                 povecavanjeKvadrata++;
             }
+
+            glavnaSkripta();
+
             //+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=
-            Console.WriteLine("Unesite broj dekadnog br. sistema:"+"\n");
-            int broj = Convert.ToInt32(Console.ReadLine()); 
-            int[] binarniZapis = new int[1];
-            int[] velicinaNiza(int x)
+            void glavnaSkripta()
             {
-                return binarniZapis = new int[x];
-            }
-            //-=-=-=-=METODA POPUNJAVANJA NIZA BINARNIM BROJEVIMA-=-=-=-=-=
-            int[] popunjavanjeNiza(int broj1, int provera1, bool binaryFormat1, int[] binarniZapis1)
-            {
-                for (int p = 0; p <= provera1; p++)
+                Console.WriteLine("Unesite broj dekadnog brojevnog sistema:");
+                int broj = Convert.ToInt32(Console.ReadLine());
+                int[] binarniZapis = new int[1];
+                int[] velicinaNiza(int x)
                 {
-                    if (broj1 == kvadratiDva[p])
-                    {
-                        //   binaryFormat1 = true;
-                        binarniZapis1[p] = 1;
-                    }
-                    else
-                    {
-                        //  binaryFormat1 = false;
-                        binarniZapis1[p] = 0;
-                    }
-                    //switch (binaryFormat1)
-                    //{
-                    //    case true:
-                    //        binarniZapis1[p] = 1;
-                    //        break;
-                    //    case false:
-                    //        binarniZapis1[p] = 2;
-                    //        break;
-                    //} 
+                    return binarniZapis = new int[x];
                 }
-                return binarniZapis1;
-            }
-            int provera = 0;
-            bool ogranicenje = false;
-            for (int i = kvadratiDva.Length - 1; i >= 0; i--)
-            {
-                bool binaryFormat = false;
-                if (broj - kvadratiDva[i] >= 0)
+                //-=-=-=-=METODA POPUNJAVANJA NIZA BINARNIM BROJEVIMA-=-=-=-=-=
+                int[] popunjavanjeNiza(int broj1, int provera1, bool binarniFormat, int[] binarniZapis1)
                 {
-                    broj -= kvadratiDva[i];
-                    provera = i;
-                    if (ogranicenje == false)
+                    for (int p = 0; p <= provera1; p++)
                     {
-                        velicinaNiza(provera + 1);
+                        if (broj1 == kvadratiDva[p])
+                        {
+                            binarniFormat = true;
+                        }
+                        else
+                        {
+                            binarniFormat = false;
+                        }
+                        switch (binarniFormat)
+                        {
+                            case true:
+                                binarniZapis1[p] = 1;
+                                break;
+                            case false:
+                                binarniZapis1[p] = 0;
+                                break;
+                        }
                     }
-                    ogranicenje = true;
-                    popunjavanjeNiza(kvadratiDva[i], provera, binaryFormat, binarniZapis);
-                } 
+                    return binarniZapis1;
+                }
+                int provera = 0;
+                bool ogranicenje = false;
+                for (int i = kvadratiDva.Length - 1; i >= 0; i--)
+                {
+                    bool binaryFormat = false;
+                    if (broj - kvadratiDva[i] >= 0)
+                    {
+                        broj -= kvadratiDva[i];
+                        provera = i;
+                        if (ogranicenje == false)
+                        {
+                            velicinaNiza(provera + 1);
+                        }
+                        ogranicenje = true;
+                        popunjavanjeNiza(kvadratiDva[i], provera, binaryFormat, binarniZapis);
+                    }
+                }
+                Console.WriteLine("Broj u binarnom formatu:");
+                Array.Reverse(binarniZapis);
+                for (int y = 0; y < binarniZapis.Length; y++)
+                {
+                    Console.Write(binarniZapis[y]);
+                }
+                Console.WriteLine("\n" +"Ponovo? (Ako želite ponovo, odmah unesite nov broj.)");
+                glavnaSkripta();
+                //if (Console.ReadLine().ToLower() == "da")
+                //{
+                //    glavnaSkripta();
+                //}
             }
-            Console.WriteLine("Broj u binarnom formatu:");
-            Array.Reverse(binarniZapis);
-            for (int y = 0; y < binarniZapis.Length; y++)
-            {
-                Console.Write(binarniZapis[y]);
-            }
+            Console.ReadLine();
         }
     }
 }
