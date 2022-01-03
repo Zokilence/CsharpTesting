ee15a1b3fd43a4364e7869d98fe07562bee8c091
Zokilence <zokicerevic@gmail.com>
2022-01-03 18:11:21 +0100

    logicki zadatak Arrays

---
+++ zadatakNizoviPremestanje/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace zadatakNizoviPremestanje
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Premestiti elemente niza tako da neprani brojevi budu na pocetku
+             Parni na kraju niza*/
+
+            Random rand = new Random();
+            int[] niz = new int[9];
+            for (int i = 0; i < 9; i++)
+            {
+                niz[i] = rand.Next(1, 51);
+            }
+
+            //main
+            for (int i = 0; i < niz.Length; i++)
+            {
+                if ()
+                {
+                     
+                }
+            }
+            //continue 
+
+
+
+         }
+    }
+}
