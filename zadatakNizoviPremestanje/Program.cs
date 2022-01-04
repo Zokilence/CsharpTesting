77ee83c7b33afffbdb26103dad915514d2e3fc98
Zokilence <zokicerevic@gmail.com>
2022-01-04 11:11:02 +0100

    premestanjeNizovi

--- zadatakNizoviPremestanje/Program.cs
+++ zadatakNizoviPremestanje/Program.cs
@@ -6,29 +6,34 @@ namespace zadatakNizoviPremestanje
     {
         static void Main(string[] args)
         {
-            /*Premestiti elemente niza tako da neprani brojevi budu na pocetku
+            /*Premestiti elemente niza tako da prani brojevi budu na pocetku
              Parni na kraju niza*/
             // 1, 3, 2, 5, 6, 8, 9, 4 -> 2,6,8,4,1,3,5,9
 
+
             Random rand = new Random();
-            int[] niz = new int[9];
-            for (int i = 0; i < 9; i++)
-            {
-                niz[i] = rand.Next(1, 51);
-            }
+            /*int[] niz = new int[9];
+              for (int i = 0; i < 9; i++)
+              {
+                  niz[i] = rand.Next(1, 51);
+              }
+            */
 
             //main
+            int[] niz = { 1, 3, 2, 5, 6, 8, 9, 4 };
+
             int a = 0;
             for (int i = 0; i < niz.Length; i++)
             {
                 if (niz[i] % 2 == 0)
                 {
-                    int temp1 = niz[a];
+                    int privremeni = niz[a];
                     niz[a] = niz[i];
-                    niz[i] = temp1;
+                    niz[i] = privremeni;
                     a++;
                 }
             }
+          
         }
     }
 }
