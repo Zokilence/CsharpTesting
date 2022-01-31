67ec696d969e9248a7d78686d2ec7de376a21ab2
Zokilence <zokicerevic@gmail.com>
2022-01-31 09:30:50 +0100

    Bubble Sort

--- CSharpTesting/Program.cs
+++ CSharpTesting/Program.cs
@@ -7,28 +7,56 @@ namespace CSharpTesting
         static void Main(string[] args)
         {
             Random rand = new Random();
-            int[] brojevi = new int[5];
+            int[] niz = new int[5];
 
-            for (int i = 0; i < brojevi.Length; i++)
+            for (int i = 0; i < niz.Length; i++)
             {
-                brojevi[i] = rand.Next(0, 31);
+                niz[i] = rand.Next(0, 31);
             }
             { }
+            /*
             //rastuci   1,3,5,2,4,6 -> 6,5,4,3,2,1 
-            for (int i = 0; i < brojevi.Length; i++)
+            for (int i = 0; i < niz.Length; i++)
             {
-                for (int j = 0; j < brojevi.Length; j++)
+                for (int j = 0; j < niz.Length; j++)
                 {
-                    if (brojevi[j] < brojevi[i])
+                    if (niz[j] < niz[i])
                     {
-                        int temp1 = brojevi[i];
-                        int temp2 = brojevi[j];
-                        brojevi[j] = temp1;
-                        brojevi[i] = temp2;
+                        int temp1 = niz[i];
+                        int temp2 = niz[j];
+                        niz[j] = temp1;
+                        niz[i] = temp2;
                     }
                 }
             }
-            foreach (var cifra in brojevi)
+            */
+            //Bubble Sort - Rastuci
+            bool provera = false;
+            int temp;
+
+            while (provera==false)
+            {
+                for (int i = 0; i < niz.Length-1; i++)
+                { 
+                    if (niz[i+1] < niz[i])
+                    {
+                        temp = niz[i+1];
+                        niz[i + 1] = niz[i];
+                        niz[i] = temp; 
+                    }
+                }
+                provera = true;
+                for (int k = 0; k < niz.Length-1; k++)
+                {
+                    if (niz[k+1] < niz[k])
+                    {
+                        provera = false;
+                    }
+                }
+            }
+             
+
+            foreach (var cifra in niz)
             {
                 Console.WriteLine(cifra.ToString());
             }
