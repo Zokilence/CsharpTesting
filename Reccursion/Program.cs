2e1041d57a3025be783f41f79c3df36bd0890622
Zokilence <zokicerevic@gmail.com>
2022-04-02 12:11:32 +0200

    RekurzijaVezbanje

---
+++ Reccursion/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Reccursion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //rekurzija faktorijel
+            int faktorijel(int ulaz)
+            {
+                if (ulaz < 2) return 1;
+                return ulaz * faktorijel(ulaz - 1);
+            }
+
+            //rekurzija broj karatkera u stringu
+            int brKaratkera(string ulaz)
+            {
+                if (ulaz == null || ulaz == "") return 0;
+                return brKaratkera(ulaz.Substring(1)) + 1;
+            }
+
+            //rekurzija stepen
+            int stepenBroja(int ulazBroj, int ulazStepen)
+            { 
+                if (ulazStepen == 1) return ulazBroj;
+                return ulazBroj * stepenBroja(ulazBroj, ulazStepen - 1);
+            }
+            Console.WriteLine(stepenBroja(1,6));
+        }
+    }
+}
