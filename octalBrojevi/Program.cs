cd2700b58534d4f08b6cec1f8f8f02f46b43cf2f
Zokilence <zokicerevic@gmail.com>
2022-07-01 11:49:43 +0200

    oktalna Konverzija

---
+++ octalBrojevi/Program.cs
@@ -0,0 +1,22 @@
+﻿using System; 
+namespace octalBrojevi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string decOctalConv(int unos)
+            {
+                string konv = "";
+                while (unos != 0)
+                {
+                    konv = konv.Insert(0, (((double)unos / 8 - Math.Truncate((double)unos / 8)) * 8).ToString());
+                    unos /= 8;
+                }
+                return konv;
+            }
+            Console.Write("Unesite broj dekadnog sistema: ");
+            Console.WriteLine($"Broj u oktalnom sistemu: {decOctalConv(Int32.Parse(Console.ReadLine()))}");
+        }
+    }
+}
